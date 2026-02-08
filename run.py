b0dd27f4f19dedcc839af2ea0bfc9a622aaa986f
christianebacani <christianbacani581@gmail.com>
2026-02-09 05:29:15 +0800

    Added new content to extract the player concurrency data and historical player statistics for every current trending games in Steam Charts.

--- run.py
+++ run.py
@@ -14,3 +14,25 @@ soup = extract_and_parse_soup(base_url)
 
 # Top 5 current trending games
 trending_games_dictionary = extract_trending_games_table(soup)
+
+trending_games_player_concurrency = []
+trending_games_player_stats = []
+
+player_concurrency_dict = {}
+historical_player_stats_dict = {}
+
+for number in range(5):
+    path = trending_games_dictionary["app_id"][number]
+    path = str(path).replace("/app", "app")
+    url = base_url + path
+
+    app_name = trending_games_dictionary["app_name"][number]
+    app_name = str(app_name)
+
+    soup = extract_and_parse_soup(url)
+
+    player_concurrency_data = extract_player_concurrency_data(soup)
+    player_concurrency_dict[app_name] = player_concurrency_data
+
+    historical_player_stats = extract_historical_player_stats(soup)
+    historical_player_stats_dict[app_name] = historical_player_stats
