f0cc5e587abca043639ffb6afd7fc4a017f167b3
christianebacani <christianbacani581@gmail.com>
2026-02-10 06:25:23 +0800

    Update the `headers` dictionary to impose 88-column threshold per lin dictionary to impose 88-column threshold per line.

--- etl/extract/extract_trending_games.py
+++ etl/extract/extract_trending_games.py
@@ -19,7 +19,8 @@ def extract_and_parse_soup(url: str) -> BeautifulSoup | None:
     :rtype: BeautifulSoup | None
     """
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
     }
     response = requests.get(url=url, headers=headers)
 
