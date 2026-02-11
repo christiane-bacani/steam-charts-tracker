5bcdfb8ee082c6522b51cd1832cdcf430376b36a
christianebacani <christianbacani581@gmail.com>
2026-02-12 06:24:12 +0800

    Add new content to the funciton `extract_and_parse_soup`

--- etl/extract/extract_top_games.py
+++ etl/extract/extract_top_games.py
@@ -17,3 +17,27 @@ def extract_and_parse_soup(url: str) -> BeautifulSoup | None:
         non-existent
     :rtype: BeautifulSoup | None
     """
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
+    }
+    response = requests.get(url=url, headers=headers)
+
+    if response.status_code != 200:
+        etl_pipeline_logs(
+            "EXTRACT",
+            "Extract and parse BeautifulSoup object",
+            "FAILED",
+            None
+        )
+        return None
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    etl_pipeline_logs(
+            "EXTRACT",
+            "Extract and parse BeautifulSoup object",
+            "SUCCESSFUL",
+            None
+    )
+    return soup
