95294e053a047dd2428679cc188cac43d3b5b999
christianebacani <christianbacani581@gmail.com>
2026-02-13 06:51:48 +0800

    Added new content to the function `extract_top_games_table`

--- etl/extract/extract_top_games.py
+++ etl/extract/extract_top_games.py
@@ -86,6 +86,16 @@ def extract_top_games_table(soup: BeautifulSoup | None) -> dict[str, list]:
         )
         table_tag = div_tag_with_content_class.find("table", attrs={"id": "top-games"})
         tbody_tag = table_tag.find("tbody")
+        list_of_all_table_row_tags = tbody_tag.find_all("tr")
+
+        for table_row_tag in list_of_all_table_row_tags:
+            list_of_all_table_data_tags = table_row_tag.find_all("td")
+
+            for cell_number, table_data_tag in enumerate(list_of_all_table_data_tags):
+                if cell_number == 0 or cell_number == 3:
+                    continue
+  
+                cell = table_data_tag.get_text()
 
     except Exception as error_message:
         etl_pipeline_logs(
