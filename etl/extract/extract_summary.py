22223eccb8abab0e947786fa93264d0ef775818e
christianebacani <christianbacani581@gmail.com>
2026-02-16 06:40:11 +0800

    Added new module

---
+++ etl/extract/extract_summary.py
@@ -0,0 +1,5 @@
+"""
+Extract the application summary of a current trending game or top game on Steam Charts.
+"""
+from bs4 import BeautifulSoup
+from logs.etl_pipeline_logs import etl_pipeline_logs
