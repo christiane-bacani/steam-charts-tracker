45c87a3d9800dc1977c8740a4161d334005f4500
christianebacani <christianbacani581@gmail.com>
2026-02-15 19:23:29 +0800

    Update the content of the docstring for every extract functions excluding the function: `extract_and_parse_soup`.

--- etl/extract/extract_trending_games.py
+++ etl/extract/extract_trending_games.py
@@ -50,8 +50,8 @@ def extract_trending_games_table(soup: BeautifulSoup | None) -> dict[str, list]:
         if non-existent
     :type soup: BeautifulSoup | None
 
-    :return: Top 5 current trending games dictionary: `{app_id: list, app_name: list,
-        change_24_h: list, current_players: list}`
+    :return: Top 5 current trending games dictionary:\n
+        `{app_id: [], app_name: [], change_24h: [], current_players: []}`
     :rtype: dict[str, list]
     """
     result = {
@@ -150,8 +150,9 @@ def extract_player_concurrency_data(
     :param trending_game_index: Current trending game index
     :type trending_game_index: int
 
-    :return: Player concurrency data dictionary: `{app_name: str, app_logo: str,
-        peak_players_24h: str, peak_players_all_time: str}`
+    :return: Player concurrency data dictionary:\n
+        `{app_name: "", app_logo: "", peak_players_24h: "",
+        peak_players_all_time: ""}`
     :rtype: dict[str, str]
     """
     result = {
@@ -270,7 +271,8 @@ def extract_historical_player_stats(
     :type trending_game_index: int
 
     :return: Historical player data dictionary:\n
-        `{period: [], avg_players: [], player_gain: [], pct_gain: [], peak_players: []}`
+        `{period: [], avg_players: [],  player_gain: [], pct_gain: [],
+        peak_players: []}`
     :rtype: dict[str, dict]
     """
     result = {
