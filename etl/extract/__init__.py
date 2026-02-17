38ae338438103c2fa2fb8c73ef7c10bfdbd1ee6e
christianebacani <christianbacani581@gmail.com>
2026-02-18 06:22:48 +0800

    Rename the the module name `extract_app_summary` to `extract_game_stats_overview`

--- etl/extract/__init__.py
+++ etl/extract/__init__.py
@@ -1,4 +1,4 @@
 from . import extract_page_content
-from . import extract_app_summary
+from . import extract_game_stats_overview
 from . import extract_trending_games
 from . import extract_top_games
