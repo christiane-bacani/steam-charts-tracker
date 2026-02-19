580fe259a84a033abfebb26b1634848351864b39
christiane_bacani <christianbacani581@gmail.com>
2026-02-20 06:06:16 +0800

    Added new function: `extract_historical_player_stats`

--- etl/extract/extract_historical_player_stats.py
+++ etl/extract/extract_historical_player_stats.py
@@ -4,3 +4,25 @@ Steam Charts.
 """
 from bs4 import BeautifulSoup
 from logs.etl_pipeline_logs import etl_pipeline_logs
+
+def extract_historical_player_stats(
+        soup: BeautifulSoup | None,
+        game_index_no: int,
+        helper: str
+) -> dict[str, list]:
+    """
+    Extract the historical player statistics table of a current trending game or
+    top game on Steam Charts.
+
+    :param soup: BeautifulSoup object representing the web-page from the url, NoneType
+        if non-existent
+    :type soup: BeautifulSoup | None
+
+    :param game_index_no: Steam Charts game index number
+    :type game_index_no: int
+
+    :return: Historical player data dictionary:\n
+        `{period_name: [], avg_no_of_players: [],  avg_no_of_players_gain: [],
+        pct_gain: [], peak_no_of_players: []}`
+    :rtype: dict[str, list]
+    """
