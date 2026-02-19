702d5a5cf03e3ca4a5bd399385da38d0c430c911
christiane_bacani <christianbacani581@gmail.com>
2026-02-20 05:54:23 +0800

    Added new module `extract_historical_player_stats`

---
+++ etl/extract/extract_historical_player_stats.py
@@ -0,0 +1,6 @@
+"""
+Extract the historical player statistics of a current trending game or top game on
+Steam Charts.
+"""
+from bs4 import BeautifulSoup
+from logs.etl_pipeline_logs import etl_pipeline_logs
