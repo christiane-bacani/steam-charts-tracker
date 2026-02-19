797dfe7b87856edd96c47ea0855d79de5a3f6429
christiane_bacani <christianbacani581@gmail.com>
2026-02-20 05:55:27 +0800

    Added new module to initialize inside the package `etl/extract/`

--- etl/extract/__init__.py
+++ etl/extract/__init__.py
@@ -1,4 +1,5 @@
 from . import extract_page_content
 from . import extract_game_stats_overview
+from . import extract_historical_player_stats
 from . import extract_trending_games
 from . import extract_top_games
