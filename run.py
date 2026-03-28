50d8ece1705e795df20010581febf4e3d20214df
christianebacani <christianbacani581@gmail.com>
2026-03-28 12:43:53 +0800

    Added new content
    1. Added BeautifulSoup library
    2. Added function "extract_top_5_trending_games" from the module
       "trending_games.py"
    3. Run the function "extract_top_5_trending_games"

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 """
 Python module to run the ETL Pipeline.
 """
+from bs4 import BeautifulSoup
+
 from etl.extract.beautiful_soup import parse_soup
+from etl.extract.trending_games import extract_top_5_trending_games
 
 # URL of the website to scrape
 url = "https://steamcharts.com/"
 
 soup = parse_soup(url)
+top_5_trending_games = extract_top_5_trending_games(soup)
