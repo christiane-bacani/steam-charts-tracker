0a1c6f0ad226114d833a2638c65f3d95c12617c5
christianebacani <christianbacani581@gmail.com>
2026-03-28 12:47:38 +0800

    Added new content
    1. Added comments to provide insights about the code snippets.
    2. Added new key to the dicitonary "result" of the function
       "extract_top_5_trending_games" to scraped another data.

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from logs.etl_pipeline_logs import provide_logs
 
 def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
+    # Navigate the web-page to get the exact HTML elements for accurate scraping
     body_tag = soup.find("body")
     div_tag_with_content_class = body_tag.find("div", attrs={"class": "content"})
 
@@ -15,7 +16,9 @@ def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
     tbody_tag = table_tag.find("tbody")
     table_row_tags = tbody_tag.find_all("tr")
 
+    # Initialize the dictionary to store the scraped data
     result = {
+        "app_id": [],
         "game_name": [],
         "twenty_four_hour_change_pct": [],
         "current_players": []
@@ -23,13 +26,21 @@ def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
 
     for table_row_tag in table_row_tags:
         table_data_tags = table_row_tag.find_all("td")
-        
+
+        # Extract the application ID
+        app_id = table_data_tags[0].find("a")["href"]
+        app_id = app_id.replace("/app/", "")
+        result["app_id"].append(app_id)
+
+        # Extract the game game
         game_name = table_data_tags[0].get_text()
         result["game_name"].append(game_name)
 
+        # Extract the percentage of 24-hour change
         twenty_four_hour_change_pct = table_data_tags[1].get_text()
         result["twenty_four_hour_change_pct"].append(twenty_four_hour_change_pct)
 
+        # Extract the number of current players
         current_players = table_data_tags[3].get_text()
         result["current_players"].append(current_players)
 
