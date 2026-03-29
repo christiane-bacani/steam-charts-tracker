07d1008a18fe1611a82255da8450d787de91af39
christianebacani <christianbacani581@gmail.com>
2026-03-29 13:22:42 +0800

    Modify the content of the module
    1. Fix the import method to only import the module and explicitly call
       it when running a function from that module
    2. Add comments for better readability
    3. Added function to scrape the stats overview for every game listed as
       a top 5 trending games from Steam Charts

--- run.py
+++ run.py
@@ -1,13 +1,29 @@
 """
 Python module to run the ETL Pipeline.
 """
-from bs4 import BeautifulSoup
-
 from etl.extract.beautiful_soup import parse_soup
-from etl.extract.trending_games import extract_top_5_trending_games
+import etl.extract.trending_games as trending_games
 
 # URL of the website to scrape
 url = "https://steamcharts.com/"
 
+# Scrape the top 5 trending games
 soup = parse_soup(url)
-top_5_trending_games = extract_top_5_trending_games(soup)
+top_5_trending_games = trending_games.extract_top_5_trending_games(soup)
+
+# Initialize a dictionary to store the scraped data of stats overview
+trending_games_stats_overview = {
+    "game_image": [],
+    "twenty_four_hour_peak_players": [],
+    "all_time_peak_players": []
+}
+
+for app_id in top_5_trending_games["app_id"]:
+    # Scrape all the stats overview and historical stats of every game listed as a
+    # top 5 trending game
+    soup = parse_soup(url + "app/" + app_id)
+
+    trending_games_stats_overview = trending_games.extract_game_stats_overview(
+        soup,
+        trending_games_stats_overview
+    )
