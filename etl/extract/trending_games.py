965970f9928ee7bc5c70349be4a9e29d3a87e5b9
christianebacani <christianbacani581@gmail.com>
2026-03-30 15:02:12 +0800

    Added new content
    1. Added new parameter to the function "extract_game_stats_overview"
    2. Added new docstring for the function "extract_game_stats_overview"
    3. Added new comment to the function "extract_game_stats_overview"
    4. Added new function "extract_historical_stats"

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -62,6 +62,7 @@ def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
 
 def extract_game_stats_overview(
         soup: BeautifulSoup,
+        app_id: str,
         trending_games_stats_overview: dict[str, list]
 ) -> dict[str, str]:
     """
@@ -70,6 +71,7 @@ def extract_game_stats_overview(
 
     Args:
         soup (BeautifulSoup): The parsed BeautifulSoup object
+        app_id (str): The application ID of a trending game
         trending_games_stats_overview (dict): The dictionary to store the scraped
             data
     Returns:
@@ -84,6 +86,9 @@ def extract_game_stats_overview(
     div_tag_with_app_heading_id = div_tag_with_content_wrapper_id.find("div", attrs={
         "id": "app-heading"
     })
+
+    # Extract the application ID of game
+    trending_games_stats_overview["app_id"].append(app_id)
 
     # Extract the image of the game as a URL path
     game_image_url_path = div_tag_with_app_heading_id.find("img")["src"]
@@ -111,3 +116,69 @@ def extract_game_stats_overview(
     trending_games_stats_overview["all_time_peak_players"].append(all_time_peak_players)
 
     return trending_games_stats_overview
+
+def extract_historical_stats(
+        soup: BeautifulSoup,
+        app_id: str,
+        historical_stats: dict[str, dict]
+) -> dict[str, dict]:
+    """
+    Scrape the historical statistics of a trending game from the Steam Charts website.
+
+    Args:
+        soup (BeautifulSoup): The parsed BeautifulSoup object
+        app_id (str): The application ID of a trending game
+        historical_stats (dict): The dictionary to store the scraped data
+    """
+    # Navigate the web-page to get the exact HTML elements for accurate scraping
+    body_tag = soup.find("body")
+    div_tag_with_content_wrapper_id = body_tag.find("div", attrs={
+        "id": "content-wrapper"
+    })
+
+    third_div_tag_with_content_classes = div_tag_with_content_wrapper_id.find_all(
+        "div",
+        attrs={
+            "class": "content"
+        }
+    )[2]
+    table_tag = third_div_tag_with_content_classes.find("table", attrs={
+        "class": "common-table"
+    })
+
+    tbody_tag = table_tag.find("tbody")
+    table_row_tags = tbody_tag.find_all("tr")
+
+    # Structure the dictionary to store the scraped data
+    historical_stats[app_id] = {
+        "month": [],
+        "avg_players": [],
+        "gain": [],
+        "gain_pct": [],
+        "peak_players": []
+    }
+
+    for table_row_tag in table_row_tags:
+        table_data_tags = table_row_tag.find_all("td")
+
+        # Extract the month
+        month = table_data_tags[0].get_text()
+        historical_stats[app_id]["month"].append(month)
+
+        # Extract the average players of the month
+        avg_players = table_data_tags[1].get_text()
+        historical_stats[app_id]["avg_players"].append(avg_players)
+
+        # Extract the number of gains of average players from previous month
+        gain = table_data_tags[2].get_text()
+        historical_stats[app_id]["gain"].append(gain)
+
+        # Extract the gain percentage of average players from previous month
+        gain_pct = table_data_tags[3].get_text()
+        historical_stats[app_id]["gain_pct"].append(gain_pct)
+
+        # Extract the no. of peak players of the month
+        peak_players = table_data_tags[4].get_text()
+        historical_stats[app_id]["peak_players"].append(peak_players)
+
+    return historical_stats
