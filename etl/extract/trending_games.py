b0ff678e1e9fb528b8bfdd237f75f4b4c1f8cb39
christianebacani <christianbacani581@gmail.com>
2026-03-31 16:27:21 +0800

    Modify the content of the module
    1. Added JSON library of Python.
    2. Fix the parameter of the extract_top_5_trending_games() function.
    3. Fix the docstring content of extract_top_5_trending_games() function.
    4. Fix the variable name from result to data from the
       extract_top_5_trending_games() function.
    5. Added new functionality to store the extracted data to a JSON file.

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -1,19 +1,17 @@
 """
 Python module to extract data about the trending games from the Steam Charts website.
 """
+import json
 from bs4 import BeautifulSoup
 
 from logs.etl_pipeline_logs import provide_logs
 
-def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
+def extract_top_5_trending_games(soup: BeautifulSoup):
     """
     Scrape the top 5 trending games from the Steam Charts website.
 
     Args:
         soup (BeautifulSoup): The parsed BeautifulSoup object
-
-    Returns:
-        dict: The scraped data of the top 5 trending games
     """
     # Navigate the web-page to get the exact HTML elements for accurate scraping
     body_tag = soup.find("body")
@@ -26,7 +24,7 @@ def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
     table_row_tags = tbody_tag.find_all("tr")
 
     # Dictionary to store the scraped data
-    result = {
+    data = {
         "app_id": [],
         "game_name": [],
         "twenty_four_hour_change_pct": [],
@@ -39,26 +37,29 @@ def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, dict]:
         # Extract the application ID
         app_id = table_data_tags[0].find("a")["href"]
         app_id = app_id.replace("/app/", "")
-        result["app_id"].append(app_id)
+        data["app_id"].append(app_id)
 
         # Extract the game game
         game_name = table_data_tags[0].get_text()
-        result["game_name"].append(game_name)
+        data["game_name"].append(game_name)
 
         # Extract the percentage of 24-hour change
         twenty_four_hour_change_pct = table_data_tags[1].get_text()
-        result["twenty_four_hour_change_pct"].append(twenty_four_hour_change_pct)
+        data["twenty_four_hour_change_pct"].append(twenty_four_hour_change_pct)
 
         # Extract the number of current players
         current_players = table_data_tags[3].get_text()
-        result["current_players"].append(current_players)
+        data["current_players"].append(current_players)
+
+    # Store the scraped data to a JSON file from `data/input` directory
+    with open("data/input/top_5_trending_games.json", "w") as file:
+        json.dump(data, file, indent=4)
 
     provide_logs(
         "EXTRACT",
         "Extract top 5 trending games from https://steamcharts.com/.",
         "Successful"
     )
-    return result
 
 def extract_game_stats_overview(
         soup: BeautifulSoup,
