f0a60c23de1fcf9dcb35f2eb23782ec70cff6813
christianebacani <christianbacani581@gmail.com>
2026-04-01 23:29:53 +0800

    Modify the content
    1. Import the module `logs.etl_pipeline`, specifically the
       provide_logs() function.
    2. Modify a comment
    3. Provide logs when running the extract_trending_games_stats_overview()
       function.

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import json
 from etl.extract.beautiful_soup import parse_soup
 import etl.extract.trending_games as trending_games
 
+from logs.etl_pipeline_logs import provide_logs
+
 # URL of the website to scrape
 url = "https://steamcharts.com/"
 
@@ -13,9 +15,7 @@ url = "https://steamcharts.com/"
 soup = parse_soup(url)
 trending_games.extract_top_5_trending_games(soup)
 
-# Get the scraped data of the top 5 trending games
-# to perform another scraping method to get their
-# stats overview and historical stats per game 
+# Get the scraped data of the top 5 trending games from the JSON file
 with open("data/input/top_5_trending_games.json", "r") as file:
     top_5_trending_games = json.load(file)
 
@@ -26,14 +26,20 @@ trending_games_stats_overview = {
     "all_time_peak_players": []
 }
 
-for app_id in top_5_trending_games["app_id"]:
-    # Scrape the stats overview and historical stats of every current trending game
+for number, app_id in enumerate(top_5_trending_games["app_id"]):
+    # Scrape the stats overview and historical stats of every current trending
+    # game from the URL
     soup = parse_soup(url + "app/" + app_id)
 
     stats_overview = trending_games.extract_trending_games_stats_overview(
         soup,
         app_id
     )
-
     for key, value in stats_overview.items():
         trending_games_stats_overview[key].append(value)
+
+    provide_logs(
+        "EXTRACT",
+        f"Extract the current top {number + 1} trending game from https://steamcharts.com/.",
+        "Successful"
+    )
