15e7848e4b8f2c86a896be5237cc321cdf7e3ee8
christiane-bacani <christianebacani@outlook.com>
2026-04-03 21:48:34 +0800

    Added new content
    Created new function save_trending_games_stats_overview_to_json()

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -121,6 +121,35 @@ def extract_trending_games_stats_overview(
     )
     return data
 
+def save_trending_games_stats_overview_to_json(
+        trending_games_stats_overview: dict[str, list],
+        filepath: str
+):
+    try:
+        with open(filepath, "w") as file:
+            json.dump(trending_games_stats_overview, file, indent=4)
+
+        provide_logs(
+            "EXTRACT",
+            "Save the extracted data of the top 5 trending games' stats "
+            "overview to a JSON file.",
+            "SUCCESSFUL",
+            None
+        )
+
+    except FileNotFoundError:
+        provide_logs(
+            "EXTRACT",
+            "Save the extracted data of the top 5 trending games' stats "
+            "overview to a JSON file.",
+            "FAILED",
+            f"Filename: '{filepath}' is not existing for saving the extracted data "
+            "of the top 5 trending game's stats overview."
+        )
+        raise FileNotFoundError("The given filename for saving the extracted data "
+                                "of the top 5 trending games' stats overview to a JSON "
+                                "file is not existing!")
+
 def extract_trending_games_historical_stats(
         soup: BeautifulSoup,
         app_id: str,
