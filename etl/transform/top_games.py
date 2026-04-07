05a7b20ee42d819a70bc237bbd2be624726cdb90
christiane-bacani <christianebacani@outlook.com>
2026-04-07 17:46:17 +0800

    Added new module

---
+++ etl/transform/top_games.py
@@ -0,0 +1,28 @@
+"""
+Python module to transform the extracted data of the current top 10 games
+(by current players) from a JSON file.
+"""
+import json
+import pandas as pd
+from datetime import datetime
+
+from logs.etl_pipeline_logs import provide_logs
+
+def transform_top_10_games(filepath: str) -> None:
+    try:
+        # Parse the scraped data from a JSON file to perform data transformation
+        with open(filepath, "r") as file:
+            top_10_games = json.load(file)
+
+    except FileNotFoundError:
+        provide_logs(
+            "TRANSFORM",
+            "Transform the extracted data of the current top 10 games "
+            "(by current players) from a JSON file.",
+            "FAILED",
+            f"Filename: '{filepath}' is invalid for parsing the extracted data "
+            "of the top 10 games (by current players) from a JSON file."
+        )
+        raise FileNotFoundError("The given filename for parsing the extracted data "
+                                "of the top 5 trending games from a JSON file is "
+                                "invalid!")
