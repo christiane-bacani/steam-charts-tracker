4a0edbf9daf72cd41729365ded6b56d7c0125d47
christiane-bacani <christianebacani@outlook.com>
2026-04-07 18:54:34 +0800

    Update contents
    - Update the comment for converting the transformed data to a DataFrame
      object and store the DataFrame to a CSV file of `data/output`
      directory.
    - Fix the content of the last argument when running the function:
      provide_logs() for transforming the extracted data of the current top
      5 trending games.
    - Update the content of the argument when raising an exception
      `FileNotFoundError` when the filepath is invalid for parsing the
      extracted data of the current top 5 trending games from a JSON file.
    
     - Added new function: transform_trending_games_stats_overview()

--- etl/transform/trending_games.py
+++ etl/transform/trending_games.py
@@ -62,7 +62,7 @@ def transform_top_5_trending_games(filepath: str) -> None:
             None
         )
         # Convert the transformed data to a DataFrame object and
-        # store the DataFrame to a CSV file from `data/output` directory
+        # store the DataFrame to a CSV file of `data/output` directory
         transformed_top_5_trending_games = pd.DataFrame(top_5_trending_games)
         transformed_top_5_trending_games.to_csv(
             "data/output/top_5_trending_games.csv",
@@ -76,8 +76,87 @@ def transform_top_5_trending_games(filepath: str) -> None:
             "from a JSON file.",
             "FAILED",
             f"Filename: '{filepath}' is invalid for parsing the extracted data "
-            "of the top 5 trending games from a JSON file."
+            "of the current top 5 trending games from a JSON file to perform data "
+            "transformation."
         )
         raise FileNotFoundError("The given filename for parsing the extracted data "
-                                "of the top 5 trending games from a JSON file is "
+                                "of the current top 5 trending games from a JSON file "
+                                "to perform data transformation is invalid!")
+
+def transform_trending_games_stats_overview(
+        filepath: str
+) -> None:
+    try:
+        # Parse the scraped data from a JSON file to perform data transformation
+        with open(filepath, "r") as file:
+            trending_games_stats_overview: dict[str, list] = json.load(file)
+
+        # Convert the datatype of all values for 'app_id' key to integer
+        for index, app_id in enumerate(
+            trending_games_stats_overview["app_id"]
+        ):
+            app_id = int(app_id)
+            trending_games_stats_overview["app_id"][index] = app_id
+
+        # Convert the datatype of all values for 'twenty_four_hour_peak_players'
+        # key to integer
+        for index, twenty_four_hour_peak_players in enumerate(
+            trending_games_stats_overview["twenty_four_hour_peak_players"]
+        ):
+            twenty_four_hour_peak_players = int(twenty_four_hour_peak_players)
+            trending_games_stats_overview[
+                "twenty_four_hour_peak_players"
+            ] = twenty_four_hour_peak_players
+
+        # Convert the datatype of all values for 'all_time_peak_players' key
+        # to integer
+        for index, all_time_peak_players in enumerate(
+            trending_games_stats_overview["all_time_peak_players"]
+        ):
+            all_time_peak_players = int(all_time_peak_players)
+            trending_games_stats_overview[
+                "all_time_peak_players"
+            ][index] = all_time_peak_players
+
+        # Convert the datatype of all values for 'current_datetime' key
+        # to datetime object with specified format
+        for index, current_datetime in enumerate(
+            trending_games_stats_overview["current_datetime"]
+        ):
+            current_datetime = datetime.strptime(
+                current_datetime,
+                "%Y-%m-%d %H:%M:%S PST%z"
+            )
+            trending_games_stats_overview["current_datetime"][index] = current_datetime
+
+        provide_logs(
+            "TRANSFORM",
+            "Transform the extracted data of the top 5 trending games' stats "
+            "overview from a JSON file.",
+            "SUCCESSFUL",
+            None
+        )
+        # Convert the transformed data to a DataFrame object and
+        # store the DataFrame to a CSV file of `data/output` directory
+        transformed_trending_games_stats_overview = pd.DataFrame(
+            trending_games_stats_overview
+        )
+        transformed_trending_games_stats_overview.to_csv(
+            "data/output/top_5_trending_games_stats_overview.json",
+            index=False
+        )
+
+    except FileNotFoundError:
+        provide_logs(
+            "TRANSFORM",
+            "Transform the extracted data of the top 5 trending games' stats "
+            "overview from a JSON file.",
+            "FAILED",
+            f"Filename: '{filepath}' is invalid for parsing the extracted data "
+            "of the top 5 trending games' stats overview from a JSON file to "
+            "perform data transformation."
+        )
+        raise FileNotFoundError("The given filename for parsing the extracted data "
+                                "of the top 5 trending games' stats overview from "
+                                "a JSON file to perform data transformation is "
                                 "invalid!")
