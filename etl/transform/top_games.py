d084582642e3669a7e21906315d0dd1020d19464
christiane-bacani <christianebacani@outlook.com>
2026-04-11 15:16:18 +0800

    Remove a library

--- etl/transform/top_games.py
+++ etl/transform/top_games.py
@@ -4,7 +4,6 @@ Python module to transform the extracted data of the current top 10 games
 """
 import json
 import pandas as pd
-from datetime import datetime
 
 from logs.etl_pipeline_logs import provide_logs
 
