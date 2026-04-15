6dc5cc8feeb7df0f48b87a426b6b389bd909e059
christiane-bacani <christianebacani@outlook.com>
2026-04-15 22:19:17 +0800

    Update the content

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -3,8 +3,10 @@
 from bs4 import BeautifulSoup
 
 def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, list]:
-    div_id_with_content_wrapper = soup.find("div", attrs={"id": "content-wrapper"})
-    div_tag_with_content_class = div_id_with_content_wrapper.find(
+    content_wrapper = soup.find("div", attrs={"id": "content-wrapper"})
+    content_class = content_wrapper.find(
         "div", attrs={"class": "content"}
     )
-    table_tag = div_tag_with_content_class.find("table")
+
+    table = content_class.find("table")
+    tbody = table.find("tbody")
