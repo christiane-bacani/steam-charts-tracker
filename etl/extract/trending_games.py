26661f8cbb5726b43ad28738ecc86ad28fdb524e
christiane-bacani <christianebacani@outlook.com>
2026-04-15 22:15:37 +0800

    Added new content

--- etl/extract/trending_games.py
+++ etl/extract/trending_games.py
@@ -3,4 +3,4 @@
 from bs4 import BeautifulSoup
 
 def extract_top_5_trending_games(soup: BeautifulSoup) -> dict[str, list]:
-    
+    div_id_with_content_wrapper = soup.find("div", attrs={"id": "content-wrapper"})
