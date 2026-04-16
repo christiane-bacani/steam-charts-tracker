f0a948ed5ba9e25f444f8b43e6579de9ab940814
christiane-bacani <christianebacani@outlook.com>
2026-04-16 16:57:46 +0800

    Added new module

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 """
 Python module to run the ETL Pipeline.
 """
+from etl.extract.trending_games import scrape_top_5_trending_games
+from utils.extract.parse import parse_soup
+
+url = "https://steamcharts.com/"
+soup = parse_soup(url)
+
+top_5_trending_games = scrape_top_5_trending_games(soup)
