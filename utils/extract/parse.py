cf2fcf9577e31351dc8b2e14441f05c5da21d11b
christiane-bacani <christianebacani@outlook.com>
2026-04-16 12:37:46 +0800

    Added new content
    Created docstring for the function: parse_soup()

--- utils/extract/parse.py
+++ utils/extract/parse.py
@@ -5,6 +5,15 @@ import requests
 from bs4 import BeautifulSoup
 
 def parse_soup(url: str) -> BeautifulSoup:
+    """
+    Parse the BeautifulSoup object to perform web-scraping.
+
+    Args:
+        url (str): URL of the website.
+
+    Returns:
+        bs4.BeautifulSoup: The parsed BeautifulSoup object for web-scraping.
+    """
     response = requests.get(url)
 
     if response.status_code != 200:
