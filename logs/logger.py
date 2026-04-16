8c637393db1fed98676296bdf7d4561aff84b888
christiane-bacani <christianebacani@outlook.com>
2026-04-16 21:01:53 +0800

    Added new module

---
+++ logs/logger.py
@@ -0,0 +1,12 @@
+"""
+Python module to create log/s per job executed inside the ETL Pipeline.
+"""
+
+def info(message: str) -> None:
+    """
+    Display logs in the terminal for the ETL Pipeline.
+
+    Args:
+        message (str): The information provided for the logs.
+    """
+    print(message)
