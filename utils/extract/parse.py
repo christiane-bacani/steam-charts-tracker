43e9820fb107e1a783d391190dcd09c379ed717b
christiane-bacani <christianebacani@outlook.com>
2026-04-17 11:20:52 +0800

    Added new content
    Added logs to the function

--- utils/extract/parse.py
+++ utils/extract/parse.py
@@ -4,6 +4,8 @@ Python module to parse objects related to extraction phase of the ETL Pipeline.
 import requests
 from bs4 import BeautifulSoup
 
+from logs import logger
+
 def parse_soup(url: str) -> BeautifulSoup:
     """
     Fetch a URL and return a parsed BeautifulSoup object.
@@ -14,10 +16,13 @@ def parse_soup(url: str) -> BeautifulSoup:
     Returns:
         BeautifulSoup: Parsed object ready for scraping.
     """
+    logger.info(f"Fetching URL: {url}")
     response = requests.get(url, timeout=10)
 
     if response.status_code != 200:
+        logger.info(f"Unsuccessfully fetched URL: {url}")
         return response.raise_for_status()
 
     else:
+        logger.info(f"Successfully fetched URL: {url}")
         return BeautifulSoup(response.text, "html.parser")
