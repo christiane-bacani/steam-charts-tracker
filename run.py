43e165e79f9ace0409b60a7bd083abe1e041fdaf
christiane-bacani <christianebacani@outlook.com>
2026-04-17 13:17:35 +0800

    Added new content
    1. Import a new function: create_database()
    2. Execute the function: create_database()

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 """
 Python module to run the ETL Pipeline.
 """
+from utils.database.database import create_database
 from utils.extract.parse import parse_soup
 
 from etl.extract.trending_games import scrape_top_5_trending_games
 
+create_database("steam_charts")
 url = "https://steamcharts.com/"
 soup = parse_soup(url)
 
