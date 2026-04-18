dd11f198105b90d695a6d94c28568cf107e475b8
christiane-bacani <christianebacani@outlook.com>
2026-04-19 00:33:32 +0800

    Update the content of the module
    1. Remove a content to the import structure.
    2. Added new loggers to the content of the function: create_database().
    3. Create autocommit for the function: create_database().
    4. Remove a content inside the if statement
    5. Added else statement

--- utils/database/database.py
+++ utils/database/database.py
@@ -3,7 +3,6 @@ Python module to create the database to store data for different layers.
 """
 import os
 from dotenv import load_dotenv
-
 from utils.database.connections import init_connection
 
 from logs import logger
@@ -16,23 +15,27 @@ def create_database(database_name: str) -> None:
     Args:
         database_name (str): The name of the database.
     """
+    logger.info("Establishing a connection to PostgreSQL to create new database.")
     load_dotenv()
     conn = init_connection(os.getenv("HOST"),
                            os.getenv("PORT"),
                            "postgres",
                            os.getenv("DB_USERNAME"),
                            os.getenv("DB_PASSWORD"))
+    conn.autocommit = True
     cursor = conn.cursor()
 
     cursor.execute(f"SELECT 1 from pg_database WHERE datname = %s", (database_name, ))
     exists = cursor.fetchone()
 
     if not exists:
-        conn.autocommit = True
         logger.info(f"Creating database: '{database_name}'.")
         cursor.execute(f"CREATE DATABASE {database_name};")
         logger.info(f"Successfully created a new database: '{database_name}'.")
         conn.autocommit = False
+
+    else:
+        logger.info(f"Database: '{database_name}' was already created.")
 
     cursor.close()
     conn.close()
