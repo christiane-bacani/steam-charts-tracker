57e6fafe3d3ebcbfef14972ebe7aaf07d4afe3c2
christiane-bacani <christianebacani@outlook.com>
2026-04-19 00:22:34 +0800

    Modify the content
    1. Added whitespaces after the library was imported from this module.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-Python module to run the ETL Pipeline.
+Python module to run the ETL Pipeline for Steam Charts Tracker.
 """
 from utils.database.database import create_database
 from utils.database.schema import create_schema
@@ -7,10 +7,12 @@ from utils.extract.parse import parse_soup
 
 from etl.extract.trending_games import scrape_top_5_trending_games
 
+
+
+create_database("steam_charts")
 create_schema("raw")
 create_schema("stg")
 create_schema("mart")
-create_database("steam_charts")
 
 url = "https://steamcharts.com/"
 soup = parse_soup(url)
