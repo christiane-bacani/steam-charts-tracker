b163afc6b1be773bd95b78da8dd1c81b329edaa5
christiane-bacani <christianebacani@outlook.com>
2026-04-19 00:11:33 +0800

    Added new content
    1. Import a new function: create_schema().
    2. Run the function: create_schema()

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 Python module to run the ETL Pipeline.
 """
 from utils.database.database import create_database
+from utils.database.schema import create_schema
 from utils.extract.parse import parse_soup
 
 from etl.extract.trending_games import scrape_top_5_trending_games
 
+create_schema("raw")
+create_schema("stg")
+create_schema("mart")
 create_database("steam_charts")
 
 url = "https://steamcharts.com/"
