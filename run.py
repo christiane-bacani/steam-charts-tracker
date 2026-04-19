80565c2c714edd2adea131353678bfd68d09f457
christiane-bacani <christianebacani@outlook.com>
2026-04-19 20:18:20 +0800

    Added new code
    1. Import the function: create_table_for_raw_layer()
    2. Run the function: create_table_for_raw_layer() to create new table
       with a name of 'top_5_trending_games' from 'steam_charts.raw' schema.

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ Python module to run the ETL Pipeline for Steam Charts Tracker.
 """
 from utils.database.database import create_database
 from utils.database.schema import create_schema
+from utils.database.table import create_table_for_raw_layer
 from utils.extract.parse import parse_soup
 
 from etl.extract.trending_games import scrape_top_5_trending_games
@@ -13,6 +14,7 @@ create_database("steam_charts")
 create_schema("raw")
 create_schema("stg")
 create_schema("mart")
+create_table_for_raw_layer("top_5_trending_games")
 
 url = "https://steamcharts.com/"
 soup = parse_soup(url)
