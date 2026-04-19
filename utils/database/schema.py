27982036ddfcd0d6e50a7d5daa944b55613bf90a
christiane-bacani <christianebacani@outlook.com>
2026-04-19 22:45:17 +0800

    Update the content
    1. Update the docstring of the module.
    2. Update the import statements.
    3. Update the docstring content of the function: create_schema().
    4. Update the content of the function: create_schema() to use SQLAlchemy
       instead of Psycopg2.

--- utils/database/schema.py
+++ utils/database/schema.py
@@ -1,6 +1,8 @@
 """
-Python module to create database schema that consist of SQL tables per data layer.
+Python module to create database schema for different data storage layer.
 """
+from sqlalchemy import text
+
 import os
 from dotenv import load_dotenv
 
@@ -10,35 +12,32 @@ from logs import logger
 
 def create_schema(schema_name: str) -> None:
     """
-    Create new schema (if still does not exist) to store all
-    data for different data storage layers.
+    Create new schema (if still does not exist) that consist of SQL tables.
 
     Args:
         schema_name (str): The name of the schema.
     """
     logger.info("Establishing a connection to PostgreSQL to create new schema.")
     load_dotenv()
-    conn = init_connection(os.getenv("HOST"),
+    engine = init_connection(os.getenv("HOST"),
                            os.getenv("PORT"),
                            "steam_charts",
                            os.getenv("DB_USERNAME"),
                            os.getenv("DB_PASSWORD"))
-    conn.autocommit = True
-    cursor = conn.cursor()
 
-    cursor.execute("""SELECT 1
-                   FROM information_schema.schemata
-                   WHERE schema_name = %s""", (schema_name, ))
-    exists = cursor.fetchone()
+    with engine.connect() as connection:
+        connection = connection.execution_options(isolation_level="AUTOCOMMIT")
 
-    if not exists:
-        logger.info(f"Creating schema: 'steam_charts.{schema_name}'.")
-        cursor.execute(f"CREATE SCHEMA {schema_name};")
-        logger.info(f"Successfully created a new schema: 'steam_charts.{schema_name}'.")
+        result = connection.execute(
+            text(f"SELECT 1 FROM information_schema.schemata WHERE schema_name =:name"),
+            {"name": schema_name}
+        )
+        exists = result.fetchone()
 
-    else:
-        logger.info(f"Schema: 'steam_charts.{schema_name}' was already created.")
+        if not exists:
+            logger.info(f"Creating schema: '{schema_name}'.")
+            connection.execute(text(f"CREATE SCHEMA {schema_name};"))
+            logger.info(f"Successfully created a new schema: '{schema_name}'.")
 
-    conn.autocommit = False
-    cursor.close()
-    conn.close()
+        else:
+            logger.info(f"Schema: '{schema_name}' was already created.")
