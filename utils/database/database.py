7f92c03720067f9a04405055775163d415e1faf7
christiane-bacani <christianebacani@outlook.com>
2026-04-19 22:25:15 +0800

    Update the content
    1. Update the docstring content of the module.
    2. Update the docstring content of the function: create_database().
    3. Update the code snippets inside the function: create_database() to
       use create_engine() function of SQLAlchemy instead of Pyscopy2 DB
       API.

--- utils/database/database.py
+++ utils/database/database.py
@@ -1,6 +1,8 @@
 """
-Python module to create the database to store data for different layers.
+Python module to create the database to store data for different data storage layers.
 """
+from sqlalchemy import text
+
 import os
 from dotenv import load_dotenv
 
@@ -11,32 +13,32 @@ from logs import logger
 def create_database(database_name: str) -> None:
     """
     Create new database (if still does not exist) to store
-    all the data for different layers.
+    all the data for different data storage layers.
 
     Args:
         database_name (str): The name of the database.
     """
     logger.info("Establishing a connection to PostgreSQL to create new database.")
     load_dotenv()
-    conn = init_connection(os.getenv("HOST"),
+    engine = init_connection(os.getenv("HOST"),
                            os.getenv("PORT"),
                            "postgres",
                            os.getenv("DB_USERNAME"),
                            os.getenv("DB_PASSWORD"))
-    conn.autocommit = True
-    cursor = conn.cursor()
 
-    cursor.execute(f"SELECT 1 from pg_database WHERE datname = %s", (database_name, ))
-    exists = cursor.fetchone()
+    with engine.connect() as connection:
+        connection = connection.execution_options(isolation_level="AUTOCOMMIT")
 
-    if not exists:
-        logger.info(f"Creating database: '{database_name}'.")
-        cursor.execute(f"CREATE DATABASE {database_name};")
-        logger.info(f"Successfully created a new database: '{database_name}'.")
-        conn.autocommit = False
+        result = connection.execute(
+            text(f"SELECT 1 from pg_database WHERE datname =:name"),
+            {"name": database_name}
+        )
+        exists = result.fetchone()
 
-    else:
-        logger.info(f"Database: '{database_name}' was already created.")
+        if not exists:
+            logger.info(f"Creating database: '{database_name}'.")
+            connection.execute(text(f"CREATE DATABASE {database_name};"))
+            logger.info(f"Successfully created a new database: '{database_name}'.")
 
-    cursor.close()
-    conn.close()
+        else:
+            logger.info(f"Database: '{database_name}' was already created.")
