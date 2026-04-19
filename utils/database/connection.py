3450778ed7cdd36ae9500256ef75db50b2585ee8
christiane-bacani <christianebacani@outlook.com>
2026-04-19 22:23:06 +0800

    Update the content
    1. Update the import statements
    2. Remove the module name when running the function create_engine()

--- utils/database/connection.py
+++ utils/database/connection.py
@@ -1,13 +1,13 @@
 """
 Python module to establish database connection to PostgreSQL.
 """
-import sqlalchemy
+from sqlalchemy import Engine, create_engine
 
 def init_connection(host: str, 
                     port: int,
                     database: str, 
                     user: str, 
-                    password: str) -> sqlalchemy.Engine:
+                    password: str) -> Engine:
     """
     Establish a PostgreSQL database connection using SQLAlchemy and Pyscopg2.
 
@@ -18,7 +18,7 @@ def init_connection(host: str,
         user (str): Username required to authenticate.
         password (str): Password used to authenticate.
     """
-    engine = sqlalchemy.create_engine(
+    engine = create_engine(
         f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
     )
     return engine
