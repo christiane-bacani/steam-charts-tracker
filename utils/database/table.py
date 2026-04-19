ca1cd2857a83769aca59b8afb7e77309e55e6f09
christiane-bacani <christianebacani@outlook.com>
2026-04-19 23:18:47 +0800

    Update the content
    1. Update the docstring content for the module.
    2. Update the import statement.
    3. Update the docstring content of the function:
       create_table_for_raw_layer().
    4. Update the code snippet of the function: create_table_for_raw_layer()
       to use SQLAlchemy as the driver instead of the psycopg2.

--- utils/database/table.py
+++ utils/database/table.py
@@ -1,6 +1,9 @@
 """
-Python module to create database table from a certain schema.
+Python module to create SQL tables from a specific schema that represents
+a specific data storage layer.
 """
+from sqlalchemy import text
+
 import os
 from dotenv import load_dotenv
 
@@ -10,42 +13,48 @@ from logs import logger
 
 def create_table_for_raw_layer(table_name: str) -> None:
     """
-    Create new table (if still does not exist) to store all
-    data from a certain data storage layer.
+    Create new SQL table (if still does not exist) from 'raw' schema
+    to store all data.
 
     Args:
         table_name (str): The name of the table.
     """
     logger.info("Establishing a connection to PostgreSQL to create new table.")
     load_dotenv()
-    conn = init_connection(os.getenv("HOST"),
+    engine = init_connection(os.getenv("HOST"),
                            os.getenv("PORT"),
                            "steam_charts",
                            os.getenv("DB_USERNAME"),
                            os.getenv("DB_PASSWORD"))
-    conn.autocommit = True
-    cursor = conn.cursor()
 
-    cursor.execute("""SELECT 1
-                   FROM information_schema.tables
-                   WHERE table_schema = %s
-                   AND table_name = %s""", ("raw", "top_5_trending_games"))
-    exists = cursor.fetchone()
+    with engine.connect() as connection:
+        connection = connection.execution_options(isolation_level="AUTOCOMMIT")
 
-    if not exists:
-        logger.info(f"Creating table: 'raw.{table_name}'.")
-        cursor.execute("""CREATE TABLE top_5_trending_games (
+        result = connection.execute(
+            text(f"""
+                 SELECT 1
+                 FROM information_schema.tables
+                 WHERE table_schema =:schema
+                 AND table_name =:table;
+            """),
+            {"schema": "raw", "table": table_name}
+        )
+        exists = result.fetchone()
+
+        if not exists:
+            logger.info(f"Creating table: '{table_name}'.")
+            connection.execute(
+                text("""
+                    CREATE TABLE raw.top5_trending_games_raw (
                     app_id VARCHAR(255),
                     rank VARCHAR(255),
                     name VARCHAR(255),
                     twenty_four_hour_change VARCHAR(255),
-                    current_players VARCHAR(255)
-                    );""")
-        logger.info(f"Successfully created a new table: 'raw.{table_name}'.")
+                    currennt_players VARCHAR(255)
+                    )
+            """)
+            )
+            logger.info(f"Successfully created a new table: '{table_name}'.")
 
-    else:
-        logger.info(f"Table: 'raw.{table_name}' was already created.")
-
-    conn.autocommit = False
-    cursor.close()
-    conn.close()
+        else:
+            logger.info(f"Table: '{table_name}' was already created.")
