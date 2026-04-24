2a0c2e33bb8c86cfeda72c944423bd68d9d8716b
christiane-bacani <christianebacani@outlook.com>
2026-04-24 22:36:12 +0800

    ADD: Libraries, modules, packages.
    Import the necessary libraries, modules, or packages for this module

--- utils/extract/top_games.py
+++ utils/extract/top_games.py
@@ -2,3 +2,11 @@
 Python module to store all the scraped data related to the current top 100 games
 (by current players) tracked by Steam Charts.
 """
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+
+from utils.database.connection import init_connection
+
+from logs import logger
