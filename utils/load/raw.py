360c17d58d19d3bd146efa755409e6c9dedc21f8
christiane-bacani <christianebacani@outlook.com>
2026-04-24 22:47:32 +0800

    ADD: Import statements
    Import the necessary modules or libraries

--- utils/load/raw.py
+++ utils/load/raw.py
@@ -2,3 +2,11 @@
 Python module to store all the scraped data that is tracked by Steam Charts to the data
 storage layer called "raw".
 """
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+
+from utils.database.connection import init_connection
+
+from logs import logger
