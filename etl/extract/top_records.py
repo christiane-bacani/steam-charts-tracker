06ac211b250273b83a519e7af74c64bc7c632ca0
christiane-bacani <christianebacani@outlook.com>
2026-04-25 20:59:41 +0800

    feat: Import statements
    Import libraries and/or packages

--- etl/extract/top_records.py
+++ etl/extract/top_records.py
@@ -2,3 +2,6 @@
 Python module to perform web-scraping to extract all the data related to
 the current top 10 records tracked by Steam Charts.
 """
+from bs4 import BeautifulSoup
+
+from logs import logger
