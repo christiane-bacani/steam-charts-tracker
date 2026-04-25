f2e8f5e8064f018aa521d52d934e1557442ddd63
christiane-bacani <christianebacani@outlook.com>
2026-04-25 21:49:44 +0800

    feat: New module
    Created a new module to extract all the data from a specific table of
    the raw data storage layefeat: New module
    
    Created a new module to extract all the data from a specific table of
    the raw data storage layer to perform data transformation.

---
+++ etl/transform/trending_games.py
@@ -0,0 +1,18 @@
+"""
+Python module to perform web-scraping to extract all the data related to
+the current top 5 trending games tracked by Steam Charts.
+"""
+import pandas as pd
+from sqlalchemy import Engine
+
+def extract_top10_games_raw(engine: Engine) -> pd.DataFrame:
+    """
+    Extract the data from the table `top10_games_raw` from the raw
+    layer.
+
+    Args:
+        engine (Engine): SQLAlchemy Engine.
+    """
+    query = "SELECT * FROM raw.top10_games_raw"
+    df = pd.read_sql(query, con=engine)
+    return df
