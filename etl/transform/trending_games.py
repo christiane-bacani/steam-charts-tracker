b2b5cb1f6ed53ae4be0daad178f59dc43063405a
christiane-bacani <christianebacani@outlook.com>
2026-04-25 22:46:53 +0800

    feat: New function: transform_top5_trending_games_raw()
    Created new function to transform the extracted data from the table
    `top5_trending_games_raw` of the raw data storage layer.

--- etl/transform/trending_games.py
+++ etl/transform/trending_games.py
@@ -29,3 +29,40 @@ def extract_top5_trending_games_raw() -> pd.DataFrame:
 
     logger.info("Successfully extracted the data from: 'top5_trending_games_raw'.")
     return df
+
+def transform_top5_trending_games_raw(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transform the extracted data from the table `top5_trending_games_raw` of the raw
+    data storage layer.
+
+    Args:
+        df (DataFrame): The extracted data as a DataFrame.
+
+    Returns:
+        DataFrame: The transformed data as a DataFrame.
+    """
+    df["app_id"] = df["app_id"].str.replace("/app/", "")
+    df["app_id"] = pd.to_numeric(df["app_id"], errors="coerce")
+
+    df["rank"].str.strip()
+    df["rank"] = pd.to_numeric(df["rank"], errors="coerce")
+
+    df["name"] = df["name"].str.strip()
+
+    df["twenty_four_hour_change"] = df["twenty_four_hour_change"].str.replace("+", "")
+    df["twenty_four_hour_change"] = df["twenty_four_hour_change"].str.replace("%", "")
+    df["twenty_four_hour_change"] = pd.to_numeric(
+        df["twenty_four_hour_change"], errors="coerce", downcast="float"
+    )
+
+    df["current_players"] = pd.to_numeric(df["current_players"], errors="coerce")
+
+    df.rename(columns={
+        "app_id":                      "application_id",
+        "rank":                        "current_rank",
+        "name":                        "game_name",
+        "twenty_four_hour_change_pct": "change_pct_within_24hr",
+        "current_players":             "no_of_current_players"
+    }, inplace=True)
+
+    return df
