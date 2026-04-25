5692b3186f9bc669f69732f3b3b4cf94f395baa4
christiane-bacani <christianebacani@outlook.com>
2026-04-25 23:45:37 +0800

    feat: Added new content to the function and new function to import.
    - Import the function: logger().
    - Added new content to the function: validate_top5_trending_games_stg().

--- etl/transform/validate.py
+++ etl/transform/validate.py
@@ -4,6 +4,8 @@ to the current top 5 trending games tracked by Steam Charts.
 """
 import pandas as pd
 
+from logs import logger
+
 def validate_top5_trending_games_stg(df: pd.DataFrame) -> pd.DataFrame:
     """
     Validate the data from the DataFrame object 'top5_trending_games_stg'
@@ -15,3 +17,23 @@ def validate_top5_trending_games_stg(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         DataFrame: The validated and transformed data as a DataFrame.
     """
+    logger.info("Validating the data from: 'top5_trending_games_stg'.")
+
+    if not pd.api.types.is_numeric_dtype(df["id"]):
+        df["id"] = pd.to_numeric(df["id"], errors="raise")
+
+    total_no_of_rows = len(df["id"])
+
+    if min(df["id"]) not in range(1, total_no_of_rows + 1):
+        raise Exception("Invalid range of values from the 'id' column!")
+
+    elif max(df["id"]) not in range(1, total_no_of_rows + 1):
+        raise Exception("Invalid range of values from the 'id' column!")
+
+    if df["id"].isnull().sum() > 0:
+        raise Exception("'id' column consist of null values!")
+
+    id_column_has_duplicates = df["id"].duplicated().any()
+
+    if id_column_has_duplicates:
+        raise Exception("'id' column consist of duplicate values!")
