fbbefef975647f7aeabc19d41c2adf355c291358
christiane-bacani <christianebacani@outlook.com>
2026-04-25 22:07:06 +0800

    refact: Content of the module
    - Added libraries/modules to import
    - Remove unnecessary docstring content to the function:
      extract_top10_games_raw().
    - Added new content to the function: extract_top10_games_raw().

--- etl/transform/trending_games.py
+++ etl/transform/trending_games.py
@@ -3,16 +3,29 @@ Python module to perform data transformation to all extracted data related
 to the current top 5 trending games tracked by Steam Charts.
 """
 import pandas as pd
-from sqlalchemy import Engine
 
-def extract_top10_games_raw(engine: Engine) -> pd.DataFrame:
+import os
+from dotenv import load_dotenv
+
+from utils.database.connection import init_connection
+
+from logs import logger
+
+def extract_top10_games_raw() -> pd.DataFrame:
     """
     Extract the data from the table `top10_games_raw` of the raw
     data storage layer.
-
-    Args:
-        engine (Engine): SQLAlchemy Engine.
     """
+    logger.info("Extracting data from the table: 'top10_games_raw'.")
+    load_dotenv()
+    engine = init_connection(os.getenv("HOST"),
+                             os.getenv("PORT"),
+                             "steam_charts",
+                             os.getenv("DB_USERNAME"),
+                             os.getenv("DB_PASSWORD"))
+
     query = "SELECT * FROM raw.top10_games_raw;"
-    df = pd.read_sql(query, con=engine)
+    df = pd.read_sql(query, engine)
+
+    logger.info("Successfully extracted the data from the table: 'top10_games_raw'.")
     return df
