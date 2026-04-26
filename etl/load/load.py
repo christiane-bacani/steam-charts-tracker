cb29c015a62fc956b2d8c33a6db24fb4049560c7
christiane-bacani <christianebacani@outlook.com>
2026-04-26 22:51:43 +0800

    feat: New function
    Created a new function with a name of `load_scraped_data_to_raw_layer()`

--- etl/load/load.py
+++ etl/load/load.py
@@ -10,3 +10,29 @@ from dotenv import load_dotenv
 from utils.database.connection import init_connection
 
 from logs import logger
+
+def load_scraped_data_to_raw_layer(scraped_data: dict,
+                                   table_name: str) -> None:
+    """
+    Load the scraped data that is tracked by Steam Charts to the data storage layer
+    called `raw`.
+
+    Args:
+        scraped_data (dict): The scraped data as a dictionary.
+        table_name: str: The name of the SQL Table.
+    """
+    logger.info("Establishing a connection to PostgreSQL to load the data to a table.")
+    load_dotenv()
+    engine = init_connection(
+        os.getenv("HOST"),
+        os.getenv("PORT"),
+        "steam_charts",
+        os.getenv("DB_USERNAME"),
+        os.getenv("DB_PASSWORD")
+    )
+
+    df = pd.DataFrame(scraped_data)
+
+    logger.info(f"Loading new data to SQL Table: '{table_name}'.")
+    df.to_sql(table_name, con=engine, schema="raw", if_exists="append", index=False)
+    logger.info(f"Successfully loaded new data to SQL table: '{table_name}'.")
