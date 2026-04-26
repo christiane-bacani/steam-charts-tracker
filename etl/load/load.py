e342bf2a856b0abe4a920f41761502c10a61da51
christiane-bacani <christianebacani@outlook.com>
2026-04-26 22:51:14 +0800

    feat: New libraries or modules
    Import libraries or modules

--- etl/load/load.py
+++ etl/load/load.py
@@ -2,3 +2,11 @@
 Python module to store all scraped, transformed, aggregrated, or modeleted data
 from Steam Charts website to their corresponding data storage layer.
 """
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+
+from utils.database.connection import init_connection
+
+from logs import logger
