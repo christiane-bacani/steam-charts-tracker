53bbb720238727189bd53c46933e1b008ba0478f
christiane-bacani <christianebacani@outlook.com>
2026-04-30 22:49:41 +0800

    refactor: Import statement structures
    Fix the structure when importing the functions from transform and
    validate module from `etl/transform` package.

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ from etl.extract.extract import scrape_top10_records
 
 from etl.extract.extract import extract_data_from_sql_table
 from etl.transform.transform import transform_top5_trending_games_raw
-from etl.transform.validate import validate_top5_trending_games_stg
 from etl.transform.transform import transform_top100_games_raw
+from etl.transform.validate import validate_top5_trending_games_stg
 from etl.transform.validate import validate_top100_games_stg
 
 from etl.load.load import load_data_to_schema
