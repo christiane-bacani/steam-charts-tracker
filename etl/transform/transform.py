cc67c524df46ca0b583b349bf8d0b18d8a0db711
christiane-bacani <christianebacani@outlook.com>
2026-05-07 00:11:05 +0800

    refactor: Content of the function: `transform_dim_column`.

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -150,13 +150,14 @@ def transform_top10_records_raw(df: pd.DataFrame) -> pd.DataFrame:
 def transform_dim_column(column_name: str) -> None:
     """
     Transform dimension columns from tables of the silver data layer to
-    create dimension tables that has business-ready structure using star schema.
+    create dimension tables that has business-ready structure using the given
+    dimension columns.
 
     Args:
         table_name (str): The name of the table.
         column_name (str): The name of the dimension column.
     """
-    logger.info("Establishing a connection to PostgreSQL to denormalize dim data.")
+    logger.info("Establishing a connection to PostgreSQL to transform dim columns.")
     load_dotenv()
     engine = init_connection(
         os.getenv("HOST"),
@@ -165,3 +166,13 @@ def transform_dim_column(column_name: str) -> None:
         os.getenv("DB_USERNAME"),
         os.getenv("DB_PASSWORD")
     )
+
+    logger.info("Transforming the dimension column: `application_id`.")
+
+    trending_games_app_id = pd.read_sql_table("top5_trending_games_stg",
+                                              con=engine,
+                                              schema="stg",
+                                              columns=[column_name])
+    print(trending_games_app_id)
+
+    logger.info("Successfully transformed the dimension column: `application_id`.")
