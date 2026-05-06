a380672727fdbeeb7b0b2c30d11d7e54fae5d346
christiane-bacani <christianebacani@outlook.com>
2026-05-07 00:22:18 +0800

    fix: Remove functions/modules.

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -3,11 +3,6 @@ Python module to perform data transformation to all data tracked by Steam Charts
 """
 import pandas as pd
 
-import os
-from dotenv import load_dotenv
-
-from utils.database.connection import init_connection
-
 from logs import logger
 
 def transform_top5_trending_games_raw(df: pd.DataFrame) -> pd.DataFrame:
@@ -146,33 +141,3 @@ def transform_top10_records_raw(df: pd.DataFrame) -> pd.DataFrame:
 
     logger.info("Successfully transformed the data from: 'top10_records_raw'.")
     return df
-
-def transform_dim_column(column_name: str) -> None:
-    """
-    Transform dimension columns from tables of the silver data layer to
-    create dimension tables that has business-ready structure using the given
-    dimension columns.
-
-    Args:
-        table_name (str): The name of the table.
-        column_name (str): The name of the dimension column.
-    """
-    logger.info("Establishing a connection to PostgreSQL to transform dim columns.")
-    load_dotenv()
-    engine = init_connection(
-        os.getenv("HOST"),
-        os.getenv("PORT"),
-        "steam_charts",
-        os.getenv("DB_USERNAME"),
-        os.getenv("DB_PASSWORD")
-    )
-
-    logger.info("Transforming the dimension column: `application_id`.")
-
-    trending_games_app_id = pd.read_sql_table("top5_trending_games_stg",
-                                              con=engine,
-                                              schema="stg",
-                                              columns=[column_name])
-    print(trending_games_app_id)
-
-    logger.info("Successfully transformed the dimension column: `application_id`.")
