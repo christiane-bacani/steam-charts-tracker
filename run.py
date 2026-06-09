08e861027ea0fd3730a3c3d69ede2fb54acaebb6
christiane-bacani <christianebacani@outlook.com>
2026-06-09 23:35:46 +0800

    feat: Created new function: 'transform_dim_peak_month'

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ from etl.transform.transform import transform_top10_records_raw
 from etl.transform.transform import transform_dim_rank_number
 from etl.transform.transform import transform_dim_steam_game
 from etl.transform.transform import transform_dim_timestamp
+from etl.transform.transform import transform_dim_peak_month
 from etl.transform.validate import validate_top5_trending_games_stg
 from etl.transform.validate import validate_top100_games_stg
 from etl.transform.validate import validate_top10_records_stg
@@ -102,3 +103,4 @@ load_data_to_schema(dim_timestamp, "mart", "dim_timestamp")
 
 # Integrate 'peak_month' dimension fron 'stg' data layer and save to 'mart' data layer
 dim_peak_month = integrate_dimension("peak_month")
+dim_peak_month = transform_dim_peak_month(dim_peak_month)
