569d2a3a087ba6fafd0da9550093416593bd676a
christiane-bacani <christianebacani@outlook.com>
2026-06-11 00:15:04 +0800

    feat: Added return statement to the docstring

--- etl/extract/integrate.py
+++ etl/extract/integrate.py
@@ -20,6 +20,9 @@ def integrate_dimension(dim_column: str) -> pd.DataFrame:
 
     Args:
         dim_column (str): The name of the dimension.
+
+    Returns:
+        DataFrame: The created dimension table.
     """
     logger.info("Establishing a connection to PostgreSQL to integrate dim columns.")
     load_dotenv()
