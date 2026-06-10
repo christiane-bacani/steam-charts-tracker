c8821c69b87e715c4bbaec72a67279bb3032839b
christiane-bacani <christianebacani@outlook.com>
2026-06-11 00:13:52 +0800

    feat: Created content from the module

--- utils/fact.py
+++ utils/fact.py
@@ -2,3 +2,17 @@
 Python module to create fact tables from silver data layer  that consist of all
 data tracked by Steam Charts.
 """
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+
+from utils.database.connection import init_connection
+
+def create_fact_table(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create fact table from a certain DataFrame object.
+
+    Args:
+        DataFrame: The created fact table.
+    """
