508fe9dad1e6c35546ba8205935a5fd8856e36d5
christiane-bacani <christianebacani@outlook.com>
2026-06-15 23:12:23 +0800

    Created a new function
    Created a new function called: `validate_fact_top_games`.

--- etl/transform/validate.py
+++ etl/transform/validate.py
@@ -779,3 +779,15 @@ def validate_fact_trending_games(df: pd.DataFrame) -> pd.DataFrame:
 
     logger.info("Successfully validated the data from: 'fact_trending_games'.")
     return df
+
+def validate_fact_top_games(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Validate the data from the DataFrame object 'fact_top_games'
+    before loading to the stage data layer.
+
+    Args:
+        df (DataFrame): The transformed data as a DataFrame.
+
+    Returns:
+        DataFrame: The validated and transformed data as a DataFrame.    
+    """
