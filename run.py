c50f0ab95da974bf45f051f8ef531d8624135fcc
christiane-bacani <christianebacani@outlook.com>
2026-06-17 17:58:42 +0800

    Added new functions to run

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ from etl.transform.transform import transform_dim_peak_month
 from etl.transform.transform import transform_dim_peak_year
 from etl.transform.transform import transform_fact_trending_games
 from etl.transform.transform import transform_fact_top_games
+from etl.transform.transform import transform_fact_top_records
 from etl.transform.validate import validate_top5_trending_games_stg
 from etl.transform.validate import validate_top100_games_stg
 from etl.transform.validate import validate_top10_records_stg
@@ -31,6 +32,7 @@ from etl.transform.validate import validate_dim_peak_month
 from etl.transform.validate import validate_dim_peak_year
 from etl.transform.validate import validate_fact_trending_games
 from etl.transform.validate import validate_fact_top_games
+from etl.transform.validate import validate_fact_top_records
 
 from etl.load.load import load_data_to_schema
 
@@ -142,3 +144,6 @@ load_data_to_schema(fact_top_games, "mart", "fact_top_games")
 # create the fact table and save to 'mart' data layer
 top10_records_stg = extract_data_from_sql_table("stg", "top10_records_stg")
 fact_top_records = create_fact_table(top10_records_stg)
+fact_top_records = transform_fact_top_records(fact_top_records)
+fact_top_records = validate_fact_top_records(fact_top_records)
+load_data_to_schema(fact_top_records, "mart", "fact_top_records")
