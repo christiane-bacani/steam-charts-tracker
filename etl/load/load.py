c4df879124bfb9096e5f083404dc27d4c9e34c24
christiane-bacani <christianebacani@outlook.com>
2026-06-17 16:17:11 +0800

    Created new SQL Command to run before loading the dimension tables to the database schema: `mart`.

--- etl/load/load.py
+++ etl/load/load.py
@@ -73,7 +73,10 @@ def load_data_to_schema(data: dict | pd.DataFrame,
                     DROP CONSTRAINT IF EXISTS fk_application_id_top_games;
 
                     ALTER TABLE mart.fact_top_games
-                    DROP CONSTRAINT IF EXISTS fk_rank_number_id_top_games;"""
+                    DROP CONSTRAINT IF EXISTS fk_rank_number_id_top_games;
+
+                    ALTER TABLE mart.fact_top_games
+                    DROP CONSTRAINT IF EXISTS fk_timestamp_id_top_games;"""
             ))
         df.to_sql(table_name,
                   con=engine,
