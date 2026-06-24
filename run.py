1a21795e13d071fff4b0bc46b3110076e698c350
christiane-bacani <christianebacani@outlook.com>
2026-06-24 21:32:05 +0800

    Added return value to the function: `transform()`.

--- run.py
+++ run.py
@@ -70,9 +70,7 @@ load(top5_trending_games_raw)
 # transform the data, validate the transformed data, and save the
 # transformed/validated data to `stg` data layer
 top100_games_raw = extract("top100_games_raw")
-print(top100_games_raw.columns)
-# transform(top100_games_raw)
-
+top100_games_raw = transform(top100_games_raw)
 """
 # Transform the extracted data of the top 100 games and save to 'stg' data layer
 top100_games_raw = extract_data_from_sql_table("raw", "top100_games_raw")
