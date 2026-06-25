e312177ea75def3804bec5202fcf52f747d7cded
christiane-bacani <christianebacani@outlook.com>
2026-06-25 23:08:50 +0800

    Fix the content of the logger messages and docstring contents

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -16,7 +16,7 @@ def transform_top5_trending_games_raw(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'top5_trending_games_raw'.")
+    logger.info("Transforming the data: 'top5_trending_games_raw'.")
 
     # Perform data transformation per column
     df["app_id"] = df["app_id"].str.replace("/app/", "")
@@ -44,7 +44,7 @@ def transform_top5_trending_games_raw(df: pd.DataFrame) -> pd.DataFrame:
         "current_players":         "no_of_current_players"
     }, inplace=True)
 
-    logger.info("Successfully transformed the data from: 'top5_trending_games_raw'.")
+    logger.info("Successfully transformed the data: 'top5_trending_games_raw'.")
     return df
 
 def transform_top100_games_raw(df: pd.DataFrame) -> pd.DataFrame:
@@ -58,7 +58,7 @@ def transform_top100_games_raw(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'top100_games_raw'.")
+    logger.info("Transforming the data: 'top100_games_raw'.")
 
     # Perform data transformation per column
     df["app_id"] = df["app_id"].str.replace("/app/", "")
@@ -85,7 +85,7 @@ def transform_top100_games_raw(df: pd.DataFrame) -> pd.DataFrame:
         "hours_played":    "no_of_hours_played",
     }, inplace=True)
 
-    logger.info("Successfully transformed the data from: 'top100_games_raw'.")
+    logger.info("Successfully transformed the data: 'top100_games_raw'.")
     return df
 
 def transform_top10_records_raw(df: pd.DataFrame) -> pd.DataFrame:
@@ -99,7 +99,7 @@ def transform_top10_records_raw(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'top10_records_raw'.")
+    logger.info("Transforming the data: 'top10_records_raw'.")
 
     # Perform data transformation per column
     df["app_id"] = df["app_id"].str.replace("/app/", "")
@@ -139,38 +139,38 @@ def transform_top10_records_raw(df: pd.DataFrame) -> pd.DataFrame:
         "peak_players": "no_of_peak_players"
     }, inplace=True)
 
-    logger.info("Successfully transformed the data from: 'top10_records_raw'.")
+    logger.info("Successfully transformed the data: 'top10_records_raw'.")
     return df
 
 def transform_dim_rank_number(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the dimension table: `dim_rank_number` from the mart data layer.
+    Transform the dimension table: `dim_rank_number`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted dimension data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'dim_rank_number'.")
+    logger.info("Transforming the data: 'dim_rank_number'.")
 
     # Type-cast the column 'rank_number'
     df["rank_number"] = pd.to_numeric(df["rank_number"], errors="coerce")
 
-    logger.info("Successfully transformed the data from: 'dim_rank_number'.")
+    logger.info("Successfully transformed the data: 'dim_rank_number'.")
     return df
 
 def transform_dim_steam_game(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the dimension table: `dim_steam_game` from the mart data layer.
+    Transform the dimension table: `dim_steam_game`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted dimension data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'dim_steam_game'.")
+    logger.info("Transforming the data: 'dim_steam_game'.")
 
     # Data deduplication
     df.drop_duplicates(keep="first", inplace=True)
@@ -181,20 +181,20 @@ def transform_dim_steam_game(df: pd.DataFrame) -> pd.DataFrame:
     # Sort the dataframe based on the application ID
     df.sort_values(by="application_id", inplace=True)
 
-    logger.info("Successfully transformed the data from: `dim_steam_game`.")
+    logger.info("Successfully transformed the data: `dim_steam_game`.")
     return df
 
 def transform_dim_timestamp(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the dimension table: `dim_timestamp` from the mart data layer.
+    Transform the dimension table: `dim_timestamp`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted dimension data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'dim_timestamp'.")
+    logger.info("Transforming the data: 'dim_timestamp'.")
 
     # Data deduplication
     df.drop_duplicates(keep="first", inplace=True)
@@ -208,20 +208,20 @@ def transform_dim_timestamp(df: pd.DataFrame) -> pd.DataFrame:
     # Reorder the structure of columns
     df = df[["id", "timestamp"]]
 
-    logger.info("Successfully transformed the data from: `dim_timestamp`.")
+    logger.info("Successfully transformed the data: `dim_timestamp`.")
     return df
 
 def transform_dim_peak_month(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the dimension table: `dim_peak_month` from the mart data layer.
+    Transform the dimension table: `dim_peak_month`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted dimension data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'dim_peak_month'.")
+    logger.info("Transforming the data: 'dim_peak_month'.")
 
     # Create the primary key
     df["id"] = range(1, len(df) + 1)
@@ -229,20 +229,20 @@ def transform_dim_peak_month(df: pd.DataFrame) -> pd.DataFrame:
     # Reorder the structure of columns
     df = df[["id", "peak_month"]]
 
-    logger.info("Successfully transformed the data from: `dim_peak_month`.")
+    logger.info("Successfully transformed the data: `dim_peak_month`.")
     return df
 
 def transform_dim_peak_year(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the dimension table: `dim_peak_year` from the mart data layer.
-    
+    Transform the dimension table: `dim_peak_year`.
+
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted dimension data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: 'dim_peak_year'.")
+    logger.info("Transforming the data: 'dim_peak_year'.")
 
     # Data deduplication
     df.drop_duplicates(keep="first", inplace=True)
@@ -259,20 +259,20 @@ def transform_dim_peak_year(df: pd.DataFrame) -> pd.DataFrame:
     # Reorder the structure of columns
     df = df[["id", "peak_year"]]
 
-    logger.info("Successfully transformed the data from: `dim_peak_year`.")
+    logger.info("Successfully transformed the data: `dim_peak_year`.")
     return df
 
 def transform_fact_trending_games(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the fact table: `fact_trending_games` from the mart data layer.
+    Transform the fact table: `fact_trending_games`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted fact data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: `fact_trending_games`.")
+    logger.info("Transforming the data: `fact_trending_games`.")
 
     # Remove rows with any missing values from any columns
     df.dropna(how="all", inplace=True, ignore_index=True)
@@ -295,20 +295,20 @@ def transform_fact_trending_games(df: pd.DataFrame) -> pd.DataFrame:
     df["timestamp_id"] = pd.to_numeric(df["timestamp_id"],
                                        errors="raise")
 
-    logger.info("Successfully transformed the data from: `fact_trending_games`.")
+    logger.info("Successfully transformed the data: `fact_trending_games`.")
     return df
 
 def transform_fact_top_games(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the fact table: `fact_top_games` from the mart data layer.
+    Transform the fact table: `fact_top_games`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted fact data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: `fact_top_games`.")
+    logger.info("Transforming the data: `fact_top_games`.")
 
     # Remove rows with any missing values from any columns
     df.dropna(how="all", inplace=True, ignore_index=True)
@@ -334,20 +334,20 @@ def transform_fact_top_games(df: pd.DataFrame) -> pd.DataFrame:
     # Type-cast the column 'no_of_hours_played'
     df["timestamp_id"] = pd.to_numeric(df["timestamp_id"], errors="raise")
 
-    logger.info("Successfully transformed the data from: `fact_top_games`.")
+    logger.info("Successfully transformed the data: `fact_top_games`.")
     return df
 
 def transform_fact_top_records(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Transform the fact table: `fact_top_records` from the mart data layer.
+    Transform the fact table: `fact_top_records`.
 
     Args:
-        df (DataFrame): The extracted data as a DataFrame.
+        df (DataFrame): The extracted fact data as a DataFrame.
 
     Returns:
         DataFrame: The transformed data as a DataFrame.
     """
-    logger.info("Transforming the data from: `fact_top_records`.")
+    logger.info("Transforming the data: `fact_top_records`.")
 
     # Type-cast the column 'application_id'
     df["application_id"] = pd.to_numeric(df["application_id"], errors="raise")
@@ -367,7 +367,7 @@ def transform_fact_top_records(df: pd.DataFrame) -> pd.DataFrame:
     # Type-cast the column 'timestamp_id'
     df["timestamp_id"] = pd.to_numeric(df["timestamp_id"], errors="raise")
 
-    logger.info("Successfully transformed the data from: `fact_top_records`.")
+    logger.info("Successfully transformed the data: `fact_top_records`.")
     return df
 
 def transform(df: pd.DataFrame) -> pd.DataFrame:
