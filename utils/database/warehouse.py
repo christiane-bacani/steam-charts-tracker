9e9895f9664a07d2aafc532196e54d651045a813
christiane-bacani <christianebacani@outlook.com>
2026-06-26 22:00:43 +0800

    Added a new content to the function: `create_warehouse()`

--- utils/database/warehouse.py
+++ utils/database/warehouse.py
@@ -19,3 +19,21 @@ def create_warehouse(warehouse_name: str) -> None:
     Args:
         warehouse_name (str): The desired name of the warehouse.
     """
+    logger.info("Establishing a connection to Snowflake to create new warehouse.")
+    load_dotenv()
+    conn = init_connection_to_snowflake(os.getenv("SNOWFLAKE_USERNAME"),
+                                        os.getenv("SNOWFLAKE_PASSWORD"),
+                                        os.getenv("SNOWFLAKE_ACCOUNT_IDENTIFIER"))
+    cursor = conn.cursor()
+
+    logger.info(f"Creating warehouse: '{warehouse_name}'.")
+    cursor.execute("""
+    CREATE WAREHOUSE IF NOT EXISTS steam_charts_warehouse
+    WITH WAREHOUSE_SIZE = 'LARGE'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    """)
+    logger.info(f"Successfully created a new warehouse: '{warehouse_name}.'")
+
+    cursor.close()
+    conn.close()
