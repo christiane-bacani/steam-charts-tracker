e68265be68715137bea42f901c140e9913c2814b
christiane-bacani <christianebacani@outlook.com>
2026-06-26 21:11:35 +0800

    Added new function: `init_connection_to_snowflake`

--- utils/database/connection.py
+++ utils/database/connection.py
@@ -1,6 +1,8 @@
 """
 Python module to establish database connection to PostgreSQL.
 """
+import snowflake.connector
+from snowflake.connector.connection import SnowflakeConnection
 from sqlalchemy import Engine, create_engine
 
 def init_connection(host: str, 
@@ -25,3 +27,24 @@ def init_connection(host: str,
         f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
     )
     return engine
+
+def init_connection_to_snowflake(warehouse: str,
+                                 database: str,
+                                 schema: str,
+                                 account: str,
+                                 user: str,
+                                 password: str) -> SnowflakeConnection:
+    """
+    Establish a Snowflake Data Warehouse connection using Snowflake Connector.
+
+    Args:
+        warehouse (str): The name of the data warehouse.
+        database (str): The name of the database.
+        schema (str): The name of the database schema.
+        account (str): Account identifier to authenticate.
+        user (str): Username required to authenticate.
+        password (str): Password used to authenticate.
+
+    Returns
+        conn (SnowflakeConnection): Snowflake Data Warehouse Connection.
+    """
