53c30d02cc7db7d15c68dec40159389638297488
christiane-bacani <christianebacani@outlook.com>
2026-08-02 02:36:30 +0800

    Fix the logic of creating a dimension table for the function: `create_dim_steam_game()`.

--- utils/dimension.py
+++ utils/dimension.py
@@ -54,17 +54,22 @@ def create_dim_steam_game(top5_trending_games_stg: pd.DataFrame,
     """
     logger.info("Creating new dimension table: 'DIM_STEAM_GAME'.")
 
-    dim_steam_game = pd.DataFrame(columns=["application_id", "game_name"])
+    pieces = []
     dataframes = [top5_trending_games_stg, top100_games_stg, top10_records_stg]
 
     for dataframe in dataframes:
-        if not dataframe["application_id"].empty and not dataframe["game_name"].empty:    
-            dim_steam_game = pd.concat([
-                dim_steam_game,
-                pd.DataFrame({"application_id": dataframe["application_id"],
-                              "game_name":      dataframe["game_name"]})
-            ],
-            ignore_index=True)
+        if not dataframe["application_id"].empty and not dataframe["game_name"].empty:
+            piece = pd.DataFrame({
+                "application_id": dataframe["application_id"],
+                "game_name":      dataframe["game_name"]
+            })
+            pieces.append(piece)
+
+    if len(pieces) > 0:
+        dim_steam_game = pd.concat(pieces, ignore_index=True)
+
+    else:
+        dim_steam_game = pd.DataFrame(columns=["application_id", "game_name"])
 
     logger.info("Successfully created a new dimension table: 'DIM_STEAM_GAME'.")
     return dim_steam_game
