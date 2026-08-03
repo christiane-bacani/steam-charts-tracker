96b95535a0acea92f28019f3d1bf06418d8de623
christiane-bacani <christianebacani@outlook.com>
2026-08-04 01:37:48 +0800

    Fix the logic for the transformation of the table: `DIM_PEAK_MONTH` for the function: `transform_dim_peak_month()`.

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -263,7 +263,7 @@ def transform_dim_peak_month(df: pd.DataFrame) -> pd.DataFrame:
     # Remove duplicate rows
     df = df.drop_duplicates(keep="first")
 
-    # Sort the dataframe based on the custom order on the 'PEAK_MONTH' column
+    # Define the custom order on the 'PEAK_MONTH' column
     custom_order = ["January", "February", "March",
                     "April",   "May",      "June",
                     "July",    "August",   "September",
@@ -271,6 +271,8 @@ def transform_dim_peak_month(df: pd.DataFrame) -> pd.DataFrame:
     df["PEAK_MONTH"] = pd.Categorical(df["PEAK_MONTH"],
                                       categories=custom_order,
                                       ordered=True)
+    # Actually apply the sort based on that custom order
+    df = df.sort_values("PEAK_MONTH").reset_index(drop=True)
 
     # Create the primary key of the dataframe
     df["ID"] = range(1, len(df) + 1)
@@ -278,9 +280,6 @@ def transform_dim_peak_month(df: pd.DataFrame) -> pd.DataFrame:
     # Reoder the column structure of the dataframe
     new_order_of_columns = ["ID", "PEAK_MONTH"]
     df = df[new_order_of_columns]
-
-    # Reset the index of the dataframe
-    df = df.reset_index(drop=True)
 
     logger.info("Successfully transformed the data: `DIM_PEAK_MONTH`.")
     return df
