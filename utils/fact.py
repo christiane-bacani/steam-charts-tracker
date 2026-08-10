1790b6e65092e1867001e693cab8d9dedcb173b4
christiane-bacani <christianebacani@outlook.com>
2026-08-10 18:05:12 +0800

    Created a new function called: `create_fact_trending_games()` to create a new fact table that references from dimension tables and use the columns from the tables of silver data layer.

--- utils/fact.py
+++ utils/fact.py
@@ -3,11 +3,13 @@ Python module to create fact tables from silver data layer that consist of all
 data tracked by Steam Charts.
 """
 import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
 
 import os
 from dotenv import load_dotenv
 
 from utils.database.connection import init_connection_to_postgres
+from utils.database.connection import init_connection_to_snowflake
 
 from logs import logger
 
@@ -49,7 +51,7 @@ def create_fact_table(df: pd.DataFrame) -> pd.DataFrame:
         FROM
             stg.top5_trending_games_stg
         INNER JOIN
-            mart.dim_steam_game  
+            mart.dim_steam_game
         ON
             stg.top5_trending_games_stg.application_id = mart.dim_steam_game.application_id
         INNER JOIN
@@ -59,8 +61,8 @@ def create_fact_table(df: pd.DataFrame) -> pd.DataFrame:
         INNER JOIN
             mart.dim_timestamp
         ON
-            stg.top5_trending_games_stg.timestamp = mart.dim_timestamp.timestamp
-        """        
+            stg.top5_trending_games_stg.timestamp = mart.dim_timestamp.timestamp;
+        """
         fact_trending_games = pd.read_sql(query, engine)
 
         logger.info("Successfully created the new fact table: 'fact_trending_games'.")
@@ -174,3 +176,89 @@ def create_fact_table(df: pd.DataFrame) -> pd.DataFrame:
 
     else:
         raise Exception("Invalid table to use for creating fact table!")
+
+def create_fact_trending_games(top5_trending_games_stgs) -> pd.DataFrame:
+    """
+    Create the fact table: `FACT_TRENDING_GAMES` using the table
+    `top5_trending_games_stg` of `stg` database schema and reference
+    the dimension data using the dimension tables.
+
+    Args:
+        top5_trending_games_stg (DataFrame): The top 5 trending games as a DataFrame.
+
+    Returns:
+        DataFrame: The created dimension table: `FACT_TRENDING_GAMES`.
+    """
+    logger.info("Creating new fact table: `FACT_TRENDING_GAMES`.")
+
+    logger.info("Establishing a connection to Snowflake to create a new fact table.")
+    load_dotenv()
+    conn = init_connection_to_snowflake(os.getenv("SNOWFLAKE_USERNAME"),
+                                        os.getenv("SNOWFLAKE_PASSWORD"),
+                                        os.getenv("SNOWFLAKE_ACCOUNT_IDENTIFIER"),
+                                        "steam_charts_warehouse",
+                                        "STEAM_CHARTS",
+                                        "MART")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE OR REPLACE TABLE STEAM_CHARTS.MART.TEMP_FACT_TRENDING_GAMES (
+    APPLICATION_ID INTEGER,
+    RANK_NUMBER_ID INTEGER,
+    CHANGE_PCT_WITHIN_24HR DECIMAL(5, 1),
+    NO_OF_CURRENT_PLAYERS INTEGER,
+    TIMESTAMP_ID INTEGER,
+    FOREIGN KEY APPLICATION_ID
+    REFERENCES STEAM_CHARTS.MART.DIM_STEAM_GAME(APPLICATION_ID),
+    FOREIGN KEY RANK_NUMBER_ID
+    REFERENCES STEAM_CHARTS.MART.RANK_NUMBER(RANK_NUMBER),
+    FOREIGN KEY TIMESTAMP_ID
+    REFERENCES STEAM_CHARTS.MART.DIM_TIMESTAMP(ID));
+    """)
+
+    query = """
+    SELECT
+        mart.dim_steam_game.application_id AS application_id,
+        mart.dim_rank_number.rank_number AS rank_number_id,
+        stg.top5_trending_games_stg.change_pct_within_24hr AS change_pct_within_24hr,
+        stg.top5_trending_games_stg.no_of_current_players AS no_of_current_players,
+        mart.dim_timestamp.id AS timestamp_id
+    FROM
+        stg.top5_trending_games_stg
+    INNER JOIN
+        mart.dim_steam_game
+    ON
+        stg.top5_trending_games_stg.application_id = mart.dim_steam_game.application_id
+    INNER JOIN
+        mart.dim_rank_number
+    ON
+        stg.top5_trending_games_stg.current_rank = mart.dim_rank_number.rank_number
+    INNER JOIN
+        mart.dim_timestamp
+    ON
+        stg.top5_trending_games_stg.timestamp = mart.dim_timestamp.timestamp;            
+    """
+    fact_trending_games = pd.read_sql(query, conn)
+
+    write_pandas(conn=conn,
+                 df=fact_trending_games,
+                 database="STEAM_CHARTS",
+                 schema="MART",
+                 table_name="TEMP_FACT_TRENDING_GAMES",
+                 auto_create_table=False,
+                 overwrite=True)
+
+    cursor.execute("""
+    DROP TABLE IF EXISTS STEAM_CHARTS.MART.FACT_TRENDING_GAMES;
+    """)
+
+    cursor.execute("""
+    ALTER TABLE STEAM_CHARTS.MART.TEMP_FACT_TRENDING_GAMES
+    RENAME TO STEAM_CHARTS.MART.FACT_TRENDING_GAMES;
+    """)
+
+    cursor.close()
+    conn.close()
+
+    logger.info(f"Successfully created a new fact table: `FACT_TRENDING_GAMES`.")
